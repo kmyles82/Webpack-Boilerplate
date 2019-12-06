f50f29846b31b40439a773b572667cce27201c73
kmyles82 <kmyles82@gmail.com>
2019-12-06 09:37:23 -0600

    config webpack.config.js file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const name = 'Dariss'
+console.log(name)
+
+class Hangman {
+    myMethod() {
+        console.log('testing')
+    }
+}
+
+const hangman = new Hangman()
+console.log(hangman.myMethod())
