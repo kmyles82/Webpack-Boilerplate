f2d07be8183efedadafd5717516d3f3245299bc9
kmyles82 <kmyles82@gmail.com>
2019-12-06 10:33:19 -0600

    testing named and default exports together

--- src/utilities.js
+++ src/utilities.js
@@ -1,7 +1,12 @@
 //Named export
-export const add = (a, b) => a + b
-export const name = 'Kerry'
+// export const add = (a, b) => a + b
+const add = (a, b) => a + b
+// export const name = 'Kerry'
+const name = 'Kerry'
 
 //Default export
 const square = (x) => x * x
-export default square;
+// export default square;
+
+//exporting Named and Default together
+export {add, name, square as default}
